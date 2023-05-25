b74ea482149213a691229ff3a20fdcbc438a9a57
Veronika <nikazemlakova38@gmail.com>
2023-05-25 20:32:22 +0300

    init components

---
+++ src/components/v-cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="v-card">
+    <h2>Its cart</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-card',
+  props: {},
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="v-catalog-item">
+    <p>item 1</p>
+    <p>price: 100</p>
+    <button>Add to cart</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-catalog-item',
+  props: {},
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
